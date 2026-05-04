119e1ac9859bca37eaec76e0b230e08f4a57fd56
Aleksandr Pavliukov <tanzoor@gmail.com>
2026-05-04 23:04:30 +0200

    remove service and repository layer in favour of actions and queries
    Co-Authored-By: Aleksandr Pavliukov <tanzoor@gmail.com>

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -4,10 +4,8 @@ declare(strict_types=1);
 
 use App\Providers\AppServiceProvider;
 use App\Providers\HorizonServiceProvider;
-use App\Providers\ModelRepositoryServiceProvider;
 
 return [
     AppServiceProvider::class,
     HorizonServiceProvider::class,
-    ModelRepositoryServiceProvider::class,
 ];
